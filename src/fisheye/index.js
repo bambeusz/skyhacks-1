539e252afc9fdd71b556082359a9d090f9064915
Daniel Chmielowiec <daniel.chmielowiec@jazzy.pro>
2018-11-18 13:22:36 +0100

    Fisheye removing

--- src/fisheye/index.js
+++ src/fisheye/index.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
+const open = require('open');
 const app = express();
 
+app.use('/jszip/', express.static(path.resolve(__dirname + '/../jszip/')));
 app.use('/fisheyegl/', express.static(path.resolve(__dirname + '/../fisheyegl/')));
 app.use('/footage/', express.static(path.resolve(__dirname + '/../footage/')));
 app.use('/', express.static(__dirname));
 
 app.listen(8080, () => {
-    console.log('listening');
+    open('http://localhost:8080');
 });
