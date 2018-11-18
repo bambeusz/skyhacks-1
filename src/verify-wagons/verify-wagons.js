de327544a6ce14f797cdcf069e3080ffa0ad6c5b
filip <filip.rozalowski@gmail.com>
2018-11-18 05:31:41 +0100

    fixes

--- src/verify-wagons/verify-wagons.js
+++ src/verify-wagons/verify-wagons.js
@@ -182,7 +182,9 @@ const getFrameFactories = (baseURL, frames) => {
 
             return verify(dir, frame)
                 .then(frame => {
+                    console.log('frame done', frame, new Date().toISOString());
                     return [...frameData, frame];
+
                 });
         });
 };
@@ -237,6 +239,7 @@ const getContainingFactories = (baseURL, containingPaths) => {
         .map(containingPath => (containingPathData) => {
             const dir = baseURL + '/' + containingPath;
             if (!fs.existsSync(dir) || !fs.lstatSync(dir).isDirectory()) {
+            	console.log(dir, new Date().toISOString());
                 return Promise.resolve(containingPathData);
             }
 
